4a3d31fc2ade1fc7ff6f20accba5c6e278f4325a
Nicolas Ochoa <nicochoaur@gmail.com>
2023-08-28 18:59:03 -0500

    fist commit counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getColor } from "./color-service";
 
@@ -6,6 +6,29 @@ export function App() {
   const [counter, setCounter] = useState(0);
   const [color, setColor] = useState("red");
 
+  const increaseNum = () => {
+    setCounter(counter + 1);
+  } 
+
+  const decreaseNum = () => {
+    setCounter(counter - 1);
+  } 
+
+  const resetNum = () => {
+    setCounter(0);
+  } 
+
+  useEffect(()=>{
+    const fetchColor = async ()=>{
+      const newColor = await getColor();
+      setColor(newColor);
+    }
+
+    if (counter >= 3) {
+      fetchColor();
+    }
+
+  });
   return (
     <div>
       <h1>Ada School - React Hooks</h1>
@@ -14,9 +37,9 @@ export function App() {
         <span className={color}>Counter:{counter}</span>
       </div>
 
-      <button>Increase</button>
-      <button>Decrease</button>
-      <button>Reset</button>
+      <button onClick={increaseNum}>Increase</button>
+      <button onClick={decreaseNum}>Decrease</button>
+      <button onClick={resetNum}>Reset</button>
     </div>
   );
 }
